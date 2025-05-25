38f16184c5b91710db01f58f0b0b1fbd173fc129
VasaviThontepu <98033976+VasaviThontepu@users.noreply.github.com>
2025-05-25 19:29:09 +0530

    Add files via upload

--- TextToVoice/TextToVoice.py
+++ TextToVoice/TextToVoice.py
@@ -3,6 +3,6 @@ from playsound import playsound
 print("=====audio is playing====")
 audio ="speech.mp3"
 language='en'
-voice=gTTS(text="my name is vasavi",lang=language,slow=False)
+voice=gTTS(text="I am Vasavi hailing from the vibrant city of Rajahmundry and Graduated my BTech degree at Aditya University ECE with overall CGPA of 8.64 ",lang=language,slow=True)
 voice.save(audio)
 playsound(audio)
